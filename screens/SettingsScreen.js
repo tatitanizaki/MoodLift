4497960ff3c53d98ba25cc5c2bd23ecebcab9366
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-04-26 16:10:03 -0400

    Addes Skills tab, fixed Title of SettingScreen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -74,16 +74,22 @@ const SettingsScreen = ({ navigation }) => {
     "Mat",
   ];
 
+  const [selectedSkills, setSelectedSkills] = useState(new Set());
+  const skillsOptions = ["Squats", "Lunges", "Push-ups"];
+
   useEffect(() => {
-    // Load saved equipment selections from local storage on mount
     const loadSavedSettings = async () => {
       try {
         const savedEquipment = await AsyncStorage.getItem("selectedEquipment");
         if (savedEquipment) {
           setSelectedEquipment(new Set(JSON.parse(savedEquipment)));
         }
+        const savedSkills = await AsyncStorage.getItem("selectedSkills");
+        if (savedSkills) {
+          setSelectedSkills(new Set(JSON.parse(savedSkills)));
+        }
       } catch (error) {
-        console.error("Failed to load equipment settings.", error);
+        console.error("Failed to load settings.", error);
       }
     };
 
@@ -98,8 +104,6 @@ const SettingsScreen = ({ navigation }) => {
       newSelection.add(equipment);
     }
     setSelectedEquipment(newSelection);
-
-    // Save the updated selections to local storage
     try {
       await AsyncStorage.setItem(
         "selectedEquipment",
@@ -110,22 +114,17 @@ const SettingsScreen = ({ navigation }) => {
     }
   };
 
-  const [selectedSkills, setSelectedSkills] = useState(new Set());
-  const skillsOptions = ["Squats", "Lunges", "Push-ups"];
-
   const handleSelectSkill = async (skill) => {
-    const newSelection = new Set(selectedSkills); // Use the correct state variable name here
+    const newSelection = new Set(selectedSkills);
     if (newSelection.has(skill)) {
       newSelection.delete(skill);
     } else {
       newSelection.add(skill);
     }
     setSelectedSkills(newSelection);
-
-    // Save the updated selections to local storage
     try {
       await AsyncStorage.setItem(
-        "selectedSkills", // Make sure the key is consistent with the state variable name
+        "selectedSkills",
         JSON.stringify([...newSelection])
       );
     } catch (error) {
@@ -133,28 +132,22 @@ const SettingsScreen = ({ navigation }) => {
     }
   };
 
-  const renderScene = ({ route }) => {
-    switch (route.key) {
-      case "equipment":
-        return (
-          <EquipmentTab
-            selectedEquipment={selectedEquipment}
-            handleSelectEquipment={handleSelectEquipment}
-            equipmentOptions={equipmentOptions}
-          />
-        );
-      case "skills":
-        return (
-          <SkillsTab
-            selectedSkills={selectedSkills}
-            handleSelectSkill={handleSelectSkill}
-            skillsOptions={skillsOptions}
-          />
-        );
-      default:
-        return null;
-    }
-  };
+  const renderScene = SceneMap({
+    equipment: () => (
+      <EquipmentTab
+        selectedEquipment={selectedEquipment}
+        handleSelectEquipment={handleSelectEquipment}
+        equipmentOptions={equipmentOptions}
+      />
+    ),
+    skills: () => (
+      <SkillsTab
+        selectedSkills={selectedSkills}
+        handleSelectSkill={handleSelectSkill}
+        skillsOptions={skillsOptions}
+      />
+    ),
+  });
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -166,20 +159,32 @@ const SettingsScreen = ({ navigation }) => {
           >
             <FontAwesome5 name="arrow-left" size={25} color="#FFFFFF" />
           </TouchableOpacity>
-          <Text style={styles.title}>Workout Settings</Text>
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <View style={styles.roundedShape}>
+              <Text style={styles.title}>Workout Settings</Text>
+            </View>
+          </View>
+          <View style={{ width: 25 }} />
         </View>
         <TabView
           navigationState={{ index, routes }}
           renderScene={renderScene}
           onIndexChange={setIndex}
           initialLayout={initialLayout}
-          renderTabBar={(props) => <TabBar {...props} style={styles.tabBar} />}
+          style={styles.tabView}
+          renderTabBar={(props) => (
+            <TabBar
+              {...props}
+              style={styles.tabBar}
+              labelStyle={styles.tabLabel}
+              indicatorStyle={styles.tabIndicator}
+            />
+          )}
         />
       </View>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
@@ -189,29 +194,31 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     alignItems: "center",
-  },
-  scrollView: {
-    width: "100%",
+    backgroundColor: "#081638",
   },
   topBar: {
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "center", // Center the title container
     width: "100%",
-    paddingVertical: 20,
-    marginTop: 50,
+    paddingTop: 40,
+    paddingBottom: 20,
+    position: "relative", // Ensure absolute positioned elements are placed relative to this view
   },
   backButton: {
-    // Define your back button styles here
+    position: "absolute", // Absolute position the back button
+    left: 20, // Place it 20 pixels from the left
+    zIndex: 10, // Make sure it's above the title container
+  },
+  scrollView: {
+    width: "100%",
   },
   roundedShape: {
     backgroundColor: "#8332ff",
     borderRadius: 20,
-    paddingHorizontal: 10,
+    paddingHorizontal: 15, // Adjust padding as needed
     paddingVertical: 5,
-    flex: 2, // Flex is used to center the title
-    justifyContent: "center", // Center content vertically
-    alignItems: "center", // Center content horizontally
-    marginLeft: 80,
+    marginLeft: 25,
   },
   appNameInsideShape: {
     fontSize: 20,
@@ -229,22 +236,17 @@ const styles = StyleSheet.create({
   equipmentLabel: {
     marginLeft: 8,
     fontSize: 16,
-    color: "#ffffff", // Or any other color suitable for your app design
+    color: "#ffffff",
   },
   sectionTitle: {
     fontSize: 18,
-    color: "#ffffff", // Or any other color suitable for your app design
+    color: "#ffffff",
     marginBottom: 16,
   },
   title: {
-    flex: 1,
-    textAlign: "center",
     fontSize: 20,
     color: "#FFFFFF",
     fontWeight: "bold",
-  },
-  tabBar: {
-    backgroundColor: "#8332ff",
   },
   tabScene: {
     padding: 10,
@@ -252,6 +254,50 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 16,
     marginLeft: 8,
+    color: "white",
+  },
+  movementContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#273864",
+    borderRadius: 15,
+    padding: 16,
+    marginBottom: 15,
+  },
+  navBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  navTitleContainer: {
+    flex: 1, // Take up available space
+    justifyContent: "center", // Center content horizontally
+    marginHorizontal: 50, // Provide space for the back button and potentially a right-side button
+  },
+  navTitle: {
+    textAlign: "center", // Ensure text is centered
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    paddingVertical: 5, // Provide vertical padding
+  },
+  tabView: {
+    flex: 1,
+    width: "100%",
+  },
+  tabBar: {
+    backgroundColor: "#081638",
+    elevation: 0,
+    shadowOpacity: 0,
+  },
+  tabLabel: {
+    color: "#FFFFFF",
+  },
+  tabIndicator: {
+    backgroundColor: "#FFFFFF",
   },
 });
 
